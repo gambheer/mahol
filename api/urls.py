af567f13296004402ce7299a1dd6eaa43e669776
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 11:14:04 +0530

    updated

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, re_path
 from .users import urls as user_urls
+from .community import urls as community_urls
+from .posts import urls as post_urls
+
 
 urlpatterns = [
     re_path('^users/', include(user_urls)),
+    re_path('^community/', include(community_urls)),
+    re_path('^posts/', include(post_urls)),
 ]
