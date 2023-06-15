7c8e904fa7de7caa84d570744d1f189084150b59
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 12:43:57 +0530

    updated

--- api/posts/get_posts.py
+++ api/posts/get_posts.py
@@ -1,6 +1,6 @@
 from api.response.base import APIResponseBase
 from api.decorators.validators import allowed_methods
-from community.models import Community
+from posts.posts_dao import PostsDao
 
 
 class GetPostsV1(APIResponseBase):
@@ -12,7 +12,15 @@ class GetPostsV1(APIResponseBase):
 
     @allowed_methods
     def get_or_create_data(self):
-        data = {}
-        communities = Community.objects.all()
-        data['communities'] = communities
+        data = {"success": True}
+        user = self.get_user(self.request.META.get('HTTP_AUTHORIZATION'))
+        if not user:
+            data = {"success": False, "message": "Invalid User"}
+            return data
+
+        _id = self.get_sanitized_int(self.request.GET.get('id'))
+        if not _id:
+            data = {"success": False, "message": "Invalid Params"}
+        post = PostsDao.get_post_by_id(_id)
+        data['post'] = post
         return data
