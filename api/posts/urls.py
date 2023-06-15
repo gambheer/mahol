167fa25909f490c65db93e604765eaeb64cd079a
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 16:17:20 +0530

    updated

--- api/posts/urls.py
+++ api/posts/urls.py
@@ -4,5 +4,5 @@ from django.views.decorators.csrf import csrf_exempt
 from . import GetPostsV1
 
 urlpatterns = [
-    re_path('^/$', csrf_exempt(GetPostsV1.as_versioned_view())),
+    re_path('^get-posts/$', csrf_exempt(GetPostsV1.as_versioned_view())),
 ]
