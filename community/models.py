7d5fb30fe02a6d284faafa3f581b01f17273e08d
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 17:10:32 +0530

    updated

--- community/models.py
+++ community/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Community(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
-    logo = models.CharField(max_length=100, blank=True, null=True)
+    logo = models.ImageField(upload_to='uploads/', max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
