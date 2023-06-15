b7d256740a6c00b60fe930b8b9fc04c27c472277
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 23:31:37 +0530

    updated

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class Users(models.Model):
     phone = models.CharField(max_length=100, blank=False, null=False)
     image = models.ImageField(upload_to='uploads/', max_length=500)
     token = models.CharField(max_length=100, blank=False, null=False)
-    role = models.IntegerField(max_length=4, blank=False, null=False)  # admin=1, super_admin=2, user=3
+    role = models.IntegerField(blank=False, null=False, default=3)  # admin=1, super_admin=2, user=3
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
