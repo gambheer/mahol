43ae682084bf533193baae511514ce649adbed49
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 10:42:04 +0530

    updated

--- posts/models.py
+++ posts/models.py
@@ -6,8 +6,8 @@ class Posts(models.Model):
     description = models.CharField(max_length=500, blank=False, null=False)
     type = models.CharField(max_length=20, blank=False, null=False)
     content = models.CharField(max_length=500, blank=False, null=False)
-    user_id = models.IntegerField(max_length=8, blank=False, null=False)
-    community_id = models.IntegerField(max_length=8, blank=False, null=False)
+    user_id = models.IntegerField(blank=False, null=False)
+    community_id = models.IntegerField(blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,4 +15,4 @@ class Posts(models.Model):
         ordering = ["created_at"]
 
     def __str__(self):
-        return self.name
+        return self.title
