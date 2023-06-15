5bdb7308fa97559d0ded387d50def7fb8aae99a4
Gambheer Singh <gambheer.singh@kukufm.com>
2023-06-15 15:46:40 +0530

    updated

--- api/users/__init__.py
+++ api/users/__init__.py
@@ -1,2 +1,3 @@
 from .get_otp import GetOtpV1
 from .verify_otp import VerifyOtpV1
+from .profile import ProfileV1
--- api/users/urls.py
+++ api/users/urls.py
@@ -3,8 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 from . import GetOtpV1
 from . import VerifyOtpV1
+from . import ProfileV1
 
 urlpatterns = [
     re_path('^auth/get-otp/$', csrf_exempt(GetOtpV1.as_versioned_view())),
     re_path('^auth/verify-otp/$', csrf_exempt(VerifyOtpV1.as_versioned_view())),
+    re_path('^profile/$', csrf_exempt(ProfileV1.as_versioned_view())),
 ]
